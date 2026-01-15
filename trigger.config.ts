d4285e45e7a1b045ce121cfef9e257cfaeff6c6f
Ramon Garate <ramonverse@proton.me>
2026-01-15 18:00:12 +0100

    fix trigger.config

--- trigger.config.ts
+++ trigger.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "@trigger.dev/sdk/v3";
 
 export default defineConfig({
-  project: "email-use",
+  project: "proj_revrgdndzlloniscwzjw",
   runtime: "node",
   logLevel: "log",
   maxDuration: 300, // 5 minutes max duration for tasks
